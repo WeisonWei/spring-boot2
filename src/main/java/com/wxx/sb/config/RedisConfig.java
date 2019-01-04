08af6a4f56f2294059b5d6a1d7ed454d58da5c81
Weision <weixx3@139.com>
2019-01-05 07:39:33 +0800

    新增redis配置
    新增redis配置

---
+++ src/main/java/com/wxx/sb/config/RedisConfig.java
@@ -0,0 +1,139 @@
+package com.wxx.sb.config;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.wxx.sb.domain.Student;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.interceptor.KeyGenerator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+import java.lang.reflect.Method;
+
+@Configuration
+@EnableCaching
+public class RedisConfig {
+
+    @Value("${spring.redis.host}")
+    private String host;
+    @Value("${spring.redis.password}")
+    private String password;
+    @Value("${spring.redis.port}")
+    private int port;
+    @Value("${spring.redis.timeout}")
+    private int timeout;
+    @Value("${spring.redis.jedis.pool.max-idle}")
+    private int maxIdle;
+    @Value("${spring.redis.jedis.pool.max-wait}")
+    private long maxWaitMillis;
+
+    @Bean
+    JedisConnectionFactory jedisConnectionFactory() {
+        JedisConnectionFactory factory = new JedisConnectionFactory();
+        factory.setHostName(host);
+        factory.setPort(port);
+        factory.setTimeout(timeout);
+        factory.setPassword(password);
+        return factory;
+    }
+
+    @Bean
+    public JedisPool redisPoolFactory() {
+        JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
+        jedisPoolConfig.setMaxIdle(maxIdle);
+        jedisPoolConfig.setMaxWaitMillis(maxWaitMillis);
+
+        JedisPool jedisPool = new JedisPool(jedisPoolConfig, host, port, timeout, password);
+        return jedisPool;
+    }
+
+
+    /**
+     * @return 自定义策略生成的key
+     * @description 自定义的缓存key的生成策略
+     * 若想使用这个key  只需要讲注解上keyGenerator的值设置为keyGenerator即可</br>
+     */
+    @Bean
+    public KeyGenerator keyGenerator() {
+        return new KeyGenerator() {
+            @Override
+            public Object generate(Object target, Method method, Object... params) {
+                StringBuffer sb = new StringBuffer();
+                sb.append(target.getClass().getName());
+                sb.append(method.getName());
+                for (Object obj : params) {
+                    sb.append(obj.toString());
+                }
+                return sb.toString();
+            }
+        };
+    }
+
+    //缓存管理器
+    @Bean
+    public RedisCacheManager cacheManager(JedisConnectionFactory jedisConnectionFactory) {
+        return RedisCacheManager.create(jedisConnectionFactory);
+    }
+
+    /**
+     * RedisTemplate配置
+     *
+     * @param jedisConnectionFactory
+     * @return
+     */
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(JedisConnectionFactory jedisConnectionFactory) {
+        //设置序列化
+        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+        ObjectMapper om = new ObjectMapper();
+        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        jackson2JsonRedisSerializer.setObjectMapper(om);
+        //配置redisTemplate
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<String, Object>();
+        redisTemplate.setConnectionFactory(jedisConnectionFactory);
+        RedisSerializer stringSerializer = new StringRedisSerializer();
+        redisTemplate.setKeySerializer(stringSerializer);//key序列化
+        redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);//value序列化
+        redisTemplate.setHashKeySerializer(stringSerializer);//Hash key序列化
+        redisTemplate.setHashValueSerializer(jackson2JsonRedisSerializer);//Hash value序列化
+        redisTemplate.afterPropertiesSet();
+        return redisTemplate;
+    }
+
+    /**
+     * RedisTemplate配置
+     *
+     * @param jedisConnectionFactory
+     * @return
+     */
+    @Bean
+    public RedisTemplate<String, Student> studentSedisTemplate(JedisConnectionFactory jedisConnectionFactory) {
+        //设置序列化
+        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+        ObjectMapper om = new ObjectMapper();
+        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        jackson2JsonRedisSerializer.setObjectMapper(om);
+        //配置redisTemplate
+        RedisTemplate<String, Student> redisTemplate = new RedisTemplate<String, Student>();
+        redisTemplate.setConnectionFactory(jedisConnectionFactory);
+        RedisSerializer stringSerializer = new StringRedisSerializer();
+        redisTemplate.setKeySerializer(stringSerializer);//key序列化
+        redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);//value序列化
+        redisTemplate.setHashKeySerializer(stringSerializer);//Hash key序列化
+        redisTemplate.setHashValueSerializer(jackson2JsonRedisSerializer);//Hash value序列化
+        redisTemplate.afterPropertiesSet();
+        return redisTemplate;
+    }
+}
--- src/main/java/com/wxx/sb/conrtoller/StudentController.java
+++ src/main/java/com/wxx/sb/conrtoller/StudentController.java
@@ -5,11 +5,14 @@ import com.wxx.sb.common.CommonUtil;
 import com.wxx.sb.domain.Student;
 import com.wxx.sb.domain.StudentXo;
 import com.wxx.sb.service.StudentService;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -28,29 +31,31 @@ public class StudentController {
     @Autowired
     private StudentService studentService;
 
-
+    @ApiOperation(value="获取用户详细信息", notes="根据url的id来获取用户详细信息")
+    @ApiImplicitParam(name = "studentXo", value = "学习信息", required = true, dataType = "StudentXo", paramType = "body")
     @RequestMapping(value = "/add", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
     public Integer addUser(@RequestBody StudentXo studentXo, HttpServletRequest request, HttpServletResponse httpServletResponse) {
         Map<String, Object> map = CommonUtil.getParameterMap(request);
         Integer integer = studentService.addStudent(CommonUtil.convertUser(studentXo));
         return integer;
     }
-
+    @ApiOperation(value="获取用户详细信息", notes="根据url的id来获取用户详细信息")
+    @ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "Integer", paramType = "path")
     @RequestMapping(value = "/delete", method = RequestMethod.DELETE, produces = "application/json;charset=UTF-8")
     public String deleteUser(@RequestBody StudentXo studentXo, HttpServletRequest request, HttpServletResponse httpServletResponse) {
         Map<String, String> map = new HashMap<>();
         List<Student> students = studentService.queryStudentList(map);
         return "";
     }
-
+    @ApiOperation(value="获取用户详细信息", notes="根据url的id来获取用户详细信息")
+    @ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "Integer", paramType = "path")
     @RequestMapping(value = "/modify", method = RequestMethod.PUT, produces = "application/json;charset=UTF-8")
     public String modifyUser(@RequestBody StudentXo studentXo, HttpServletRequest request, HttpServletResponse httpServletResponse) {
         Map<String, String> map = new HashMap<>();
         List<Student> students = studentService.queryStudentList(map);
         return "";
     }
-
-    @RequestMapping(value = "/query", method = RequestMethod.GET, produces = "application/json;charset=UTF-8")
+    @ApiIgnore//使用该注解忽略这个API
     public String queryUser(HttpServletRequest request, HttpServletResponse httpServletResponse) {
         Map<String, String> map = new HashMap<>();
         List<Student> students = studentService.queryStudentList(map);
